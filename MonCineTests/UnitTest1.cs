7907b6d14c2decd9b69b2b122019aebd7170b989
Melina Hotte <melina.hotte@gmail.com>
2022-04-06 16:40:35 -0400

    Ajout du projet de tests unitaires

---
+++ MonCineTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using MonCine.Data;
+using Xunit;
+
+namespace MonCineTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Abonne a = new Abonne();
+        }
+    }
+}
